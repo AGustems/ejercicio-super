76a723591bba3856ae0d1bdb74561d6f1a190b9c
Alba Gustems <albagustems@gmail.com>
2020-05-21 20:42:31 +0200

    final configuration

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
 const cors = require("cors");
-const {urlencoded, json} = express;
+const frutasRoute = require("./routes/frutasRoute");
+//const carneRoute = require("./routes/carneRoute");
+//const galletasRoute = require("./routes/galletasRoute");
+//const pizzaRoute = require("./routes/pizzaRoute");
+//const chocolateRoute = require("./routes/chocolateRoute");
+//const verduraRoute = require("./routes/verduraRoute");
+
+const {
+    urlencoded,
+    json
+} = express;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(urlencoded({extended: true}))
+app.use(urlencoded({
+    extended: true
+}))
 app.use(json())
 app.use(cors())
 
@@ -16,16 +28,16 @@ app.set("views", path.join(__dirname, "views"));
 
 hbs.registerPartials(path.join(__dirname, "views/partials"));
 
-app.get("/", (req, res, next)=>{
+app.get("/", (req, res, next) => {
     res.render("index")
 })
 
-app.use("/fruta", frutaRoute);
-app.use("/carne", carneRoute);
-app.use("/galletas", galletasRoute);
-app.use("/pizza", pizzaRoute);
-app.use("/chocolate", chocolateRoute);
-app.use("/verdura", verduraRoute);
+app.use("/frutas", frutasRoute);
+//app.use("/carne", carneRoute);
+//app.use("/galletas", galletasRoute);
+//app.use("/pizza", pizzaRoute);
+//app.use("/chocolate", chocolateRoute);
+//app.use("/verdura", verduraRoute);
 
 app.listen(3000, () => {
     console.log("Server running")
